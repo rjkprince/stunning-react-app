c50ae8d1ecb9aeb5dd88557e00087fd57c71f031
Rajesh kumar <prince.rk.cool@gmail.com>
2020-09-01 12:10:21 +0530

    Updates

---
+++ precache-manifest.ac9721eafb62aaa08bb975acec525353.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f47dfaf4df7637c86f7ae5b704370a4",
+    "url": "/stunning-react-app/index.html"
+  },
+  {
+    "revision": "d34c79334d6767b2600e",
+    "url": "/stunning-react-app/static/css/main.99ac3556.chunk.css"
+  },
+  {
+    "revision": "f53446708f39ea612177",
+    "url": "/stunning-react-app/static/js/2.f15af276.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/stunning-react-app/static/js/2.f15af276.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34c79334d6767b2600e",
+    "url": "/stunning-react-app/static/js/main.c6bf00c5.chunk.js"
+  },
+  {
+    "revision": "aadf64f8d7b240bdeae1",
+    "url": "/stunning-react-app/static/js/runtime-main.035d7486.js"
+  },
+  {
+    "revision": "adfc2ab40e78eb51b47729785de19c1f",
+    "url": "/stunning-react-app/static/media/arrow-down.adfc2ab4.png"
+  },
+  {
+    "revision": "881e4ec8e9e4ce91cacc8cf8475eb789",
+    "url": "/stunning-react-app/static/media/arrow-up.881e4ec8.png"
+  },
+  {
+    "revision": "5044cc0bc6e36b600c32c1772ae813b7",
+    "url": "/stunning-react-app/static/media/landing-section-image.5044cc0b.png"
+  },
+  {
+    "revision": "d43e2fd40a220c8a0aff6eb9181cc2a3",
+    "url": "/stunning-react-app/static/media/stunning-logo.d43e2fd4.png"
+  },
+  {
+    "revision": "330ba7b090d196001b486bfb9ed8a19e",
+    "url": "/stunning-react-app/static/media/tick-blue.330ba7b0.png"
+  },
+  {
+    "revision": "289635ccdbc5a4b5f1468d8459e632b8",
+    "url": "/stunning-react-app/static/media/tick-gray.289635cc.png"
+  },
+  {
+    "revision": "6bf196c58f593de5fc5e5bf4eaca5873",
+    "url": "/stunning-react-app/static/media/user-blue.6bf196c5.png"
+  },
+  {
+    "revision": "29fbde9dc87aab174b40152545b6e1fe",
+    "url": "/stunning-react-app/static/media/user-orange.29fbde9d.png"
+  },
+  {
+    "revision": "9f9b7199e6173f137432ece1c8bf2522",
+    "url": "/stunning-react-app/static/media/user-pink.9f9b7199.png"
+  },
+  {
+    "revision": "aeb86ae0f05c13486c9786f58befa393",
+    "url": "/stunning-react-app/static/media/user-red.aeb86ae0.png"
+  }
+]);
